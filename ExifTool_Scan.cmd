8ae76e8f7c84e7e0ed6cdf8c4cdf1709631ea872
James Grace <james.n.grace@gmail.com>
2021-08-18 12:42:45 -0400

    Create ExifTool_Scan.cmd

---
+++ ExifTool_Scan.cmd
@@ -0,0 +1,45 @@
+@echo off
+
+REM - - - - - - - - - - - - version 18august2021 - - -
+REM
+REM
+REM  ExifTool - https://exiftool.org/install.html
+REM
+REM  Blat (64bit) - https://www.blat.net
+REM
+REM
+REM - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+
+set exiftoollocation=C:\Windows
+
+set blatlocation=C:\Program Files\Blat
+
+set scanfolder=D:\ASSETS
+
+set scanlogfolder=D:\ExifTool Scan
+
+set scanlogfilename=ExifTool_Scan_Log.txt
+
+set emailserver=mail.example.com
+
+set emailto=recipient@example.com
+
+set emailfrom=server_noreply@example.com
+
+set emailsubject="Automation Notification: ExifTool Scan Log"
+
+
+REM - - - - - - - - - - - - - - - - - - - - - - - - -
+REM - - - - - - - - - - - - - - - - - - - - - - - - -
+
+
+del "%scanlogfolder%\%scanlogfilename%" /Q
+
+> "%scanlogfolder%\%scanlogfilename%" 2>&1 (
+
+"%exiftoollocation%\exiftool.exe" -m -p "$directory/$filename" -r -if "$documentancestors and (()=$documentancestors =~ /, /g) > 100" "%scanfolder%"
+
+)
+
+"%blatlocation%\blat.exe" "%scanlogfolder%\%scanlogfilename%" -to %emailto% -f %emailfrom% -s %emailsubject% -server %emailserver%
